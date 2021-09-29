95387885a0373de82faac9c00d63f6c417f70f89
Florian Labarre <florian.labarre@siatech.fr>
2021-09-29 10:30:38 +0200

    Merge serve.sh and restore.sh

--- restore.sh
+++
@@ -1,7 +0,0 @@
-ip=$(ipconfig | grep "Adresse IPv4" | sed 's/ /\n/gmi' | grep 192.168.1 | sed 's/\s//gmi' | head -n1)
-
-rm apps-local.json
-from="http://$ip:8321"
-to="https://raw.githubusercontent.com/siatech/nrf-tools/main"
-sed -i "s#$from#$to#g" comhand-configurator
-sed -i "s#$from#$to#g" siatech-ble-updater
--- serve.sh
+++ serve.sh
@@ -1,13 +1,42 @@
+#!/bin/sh
+
 ip=$(ipconfig | grep "Adresse IPv4" | sed 's/ /\n/gmi' | grep 192.168.1 | sed 's/\s//gmi' | head -n1)
+port=8321
+localUrl="http://$ip:$port"
+remoteUrl="https://raw.githubusercontent.com/siatech/nrf-tools/main"
+
+# this function is called when Ctrl-C is sent
+function trap_ctrlc ()
+{
+    # perform cleanup here
+    echo "Ctrl-C caught...performing clean up"
+ 
+    rm apps-local.json
+    from=$localUrl
+    to=$remoteUrl
+    sed -i "s#$from#$to#g" comhand-configurator
+    sed -i "s#$from#$to#g" siatech-ble-updater
+
+
+    echo "Cleanup done."
+ 
+    # exit shell script with error code 2
+    # if omitted, shell script will continue execution
+    exit 2
+}
+ 
+# initialise trap to call trap_ctrlc function
+# when signal 2 (SIGINT) is received
+trap "trap_ctrlc" 2
 
 cp apps.json apps-local.json
-from="https://raw.githubusercontent.com/siatech/nrf-tools/main"
-to="http://$ip:8321"
+from=$remoteUrl
+to=$localUrl
 sed -i "s#$from#$to#g" apps-local.json
 sed -i "s#$from#$to#g" comhand-configurator
 sed -i "s#$from#$to#g" siatech-ble-updater
 sed -i 's#"siatech"#"siatech-local"#g' apps-local.json
 
-echo "http://$ip:8321/apps-local.json"
+echo "$localUrl/apps-local.json"
 
-python -m http.server 8321 --bind $ip
+python -m http.server $port --bind $ip
